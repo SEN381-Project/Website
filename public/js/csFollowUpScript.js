3069642a2799b5c0b01a17d652b47e7d2613d720
Tebogo Gerald Nkwane <nkwane.gerald@gmail.com>
2021-05-16 15:57:26 +0200

    JSON object creation

--- public/js/csFollowUpScript.js
+++ public/js/csFollowUpScript.js
@@ -8,15 +8,31 @@ function OpenSetReminderSite()
 
 function InsertFollowUP(id, status, date) {
     console.log(`Insert this ${id} ${status} ${date}`);
+    let data = {
+        id: id,
+        status: status,
+        date: date
+    }
+    console.log(data);
     // POST request
 }
 
 function UpdateFollowUP(id, status, date) {
     console.log(`Update this ${id} ${status} ${date}`);
+    let data = {
+        id: id,
+        status: status,
+        date: date
+    }
+    console.log(data);
     // PATCH request
 }
 
 function DeleteFollowUP(id) {
     console.log(`Delete this ${id}`);
+    let data = {
+        id: id
+    }
+    console.log(data);
     // DELETE request 
 }

5952538a58e5b966b79256d876b58418267135d8
Tebogo Gerald Nkwane <nkwane.gerald@gmail.com>
2021-05-16 15:49:00 +0200

    CRUD operations setup

--- public/js/csFollowUpScript.js
+++ public/js/csFollowUpScript.js
@@ -8,12 +8,15 @@ function OpenSetReminderSite()
 
 function InsertFollowUP(id, status, date) {
     console.log(`Insert this ${id} ${status} ${date}`);
+    // POST request
 }
 
 function UpdateFollowUP(id, status, date) {
     console.log(`Update this ${id} ${status} ${date}`);
+    // PATCH request
 }
 
 function DeleteFollowUP(id) {
     console.log(`Delete this ${id}`);
+    // DELETE request 
 }

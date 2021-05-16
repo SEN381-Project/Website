408042d5476b7e27630386a482845f72efa32795
Tebogo Gerald Nkwane <nkwane.gerald@gmail.com>
2021-05-16 15:07:02 +0200

    Initial Follow Up Functionality

--- public/js/csFollowUpScript.js
+++ public/js/csFollowUpScript.js
@@ -1,8 +1,23 @@
 document.getElementById("btnSetReminder").onclick = OpenSetReminderSite;
-
+document.getElementById("btnInsert").onclick = InsertFollowUP;
+document.getElementById("btnUpdate").onclick = UpdateFollowUP;
+document.getElementById("btnDelete").onclick = DeleteFollowUP;
 
 function OpenSetReminderSite() 
 {
     alert("Redirecting to the 'set reminder' form");
     window.location.replace("/csSetReminder");
+}
+
+function InsertFollowUP() {
+    console.log("Insert object here");
+    
+}
+
+function UpdateFollowUP() {
+    console.log("Update object here");
+}
+
+function DeleteFollowUP() {
+    console.log("Delete object here");
 }

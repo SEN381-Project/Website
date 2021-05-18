fd70050c4c4b665a73c8a7a2146b8c2150f9c930
Tian Bornman <tianbornman.tb@gmail.com>
2021-05-18 19:54:13 +0200

    made reminder js stuff woweeee

--- public/js/csSetReminderScript.js
+++ public/js/csSetReminderScript.js
@@ -9,30 +9,68 @@ function DisplayCurrentTime()
     document.getElementById("currentTimeDisplay").innerHTML = "<u>Current time</u> = " + Today.getHours() + " : " + Today.getMinutes() + " : " + Today.getSeconds();
 }
 
-function Insert(id, text, date) 
+async function Insert(id, text, date) 
 {
     alert("Inserting: " + id + ", " + text + ", " + date);
     let obj = {
-        id: id,
-        text: text,
-        date: date
+        ReminderText: {
+            type: text
+        },
+        ReminderDate: {
+            type: date
+        }
     }
+    const options = {         
+        method: 'POST',         
+        headers: {'Content-Type': 'application/json'},         
+        body: JSON.stringify(obj)    
+    }      
+    const response = await fetch('http://localhost:3000/db/reminder', options);
+    const responseData = await response.json();
+    console.log(responseData);
 }
 
-function Update(id, text, date) 
+async function Update(id, text, date) 
 {
     alert("Updating: " + id + ", to: " + text + ", " + date);
     let obj = {
-        id: id,
-        text: text,
-        date: date
+        ReminderText: {
+            type: text
+        },
+        ReminderDate: {
+            type: date
+        }
     }
+
+    const options = {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj)
+    };
+    const finalLink = 'http://localhost:3000/db/reminder/' + id;
+    const response = await fetch(finalLink, options);
+    const responseData = await response.json();
+    console.log(responseData);
 }
 
-function Delete(id) 
+async function Delete(id) 
 {
     alert("Deleting: " + id);
     let obj = {
-        id: id,
+        id: id
     }
+
+    const options = {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj)
+    };
+    const finalLink = 'http://localhost:3000/db/reminder/' + id;
+    const response = await fetch(finalLink, options);
+    const responseData = await response.json();
+    console.log(responseData);
 }
